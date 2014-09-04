a445976764caba8a4fca4a0adb9893666a116aaa
Klaus Ganser <kganser@gmail.com>
2014-09-04 08:25:06 -0500

    Generate markup in background page; add links

--- extension/jsonv.js
+++ extension/jsonv.js
@@ -2,62 +2,35 @@
 // MIT Licensed, with this copyright and permission notice
 // <http://opensource.org/licenses/MIT>
 
-var jsml = (function(node, parent) {
-  var attr = function(node, parent) {
-    Object.keys(node).forEach(function(k) {
-      if (k == 'children') return;
-      var n = node[k];
-      if (typeof parent[k] == 'undefined' || typeof n != 'object' || n == null)
-        return parent[k] = n;
-      attr(n, parent[k], true);
+if (location.protocol == 'chrome-extension:') {
+  chrome.runtime.onConnect.addListener(function(client) {
+    client.onMessage.addListener(function(json) {
+      try {
+        client.postMessage(function html(data) {
+          var type = Array.isArray(data) ? 'array' : typeof data == 'object' ? data ? 'object' : 'null' : typeof data;
+          return '<span class="jsonv-'+type+'">'+(type == 'array'
+            ? '<ol>'+data.map(function(e) { return '<li>'+html(e)+'</li>'; }).join('')+'</ol>'
+            : type == 'object'
+              ? '<ul>'+Object.keys(data).map(function(key) { return '<li><span class="jsonv-key">'+key.replace(/&/g, '&amp;').replace(/</g, '&lt;')+'</span>'+html(data[key])+'</li>'; }).join('')+'</ul>'
+              : String(data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/^(https?:\/\/[^\s]+)$/, '<a href="$1">$1</a>')
+          )+'</span>';
+        }(JSON.parse(json)));
+      } catch (e) {}
     });
-    return node.children;
-  };
-  
-  return function(node, parent) {
-    switch (typeof node) {
-      case 'object':
-        if (!node) return;
-        if (Array.isArray(node)) {
-          node.forEach(function(node) { jsml(node, parent); });
-          return parent;
-        }
-        var tag = Object.keys(node)[0],
-            elem = document.createElement(tag);
-        if (parent) parent.appendChild(elem); 
-        node = node[tag];
-        jsml(typeof node == 'object' && node && !Array.isArray(node) ? attr(node, elem) : node, elem);
-        return elem;
-      case 'function':
-        return jsml(node(parent), parent);
-      case 'string':
-      case 'number':
-        node = document.createTextNode(node);
-        return parent ? parent.appendChild(node) : node;
-    }
-  };
-})();
-
-var jsonv = function(data) {
-  var type = Array.isArray(data) ? 'array' : typeof data == 'object' ? data ? 'object' : 'null' : typeof data;
-  return {span: {className: 'jsonv-'+type, children: type == 'array'
-    ? {ol: data.map(function(e) { return {li: [jsonv(e)]}; })}
-    : type == 'object'
-      ? {ul: Object.keys(data).map(function(key) { return {li: [{span: {className: 'jsonv-key', children: key}}, jsonv(data[key])]}; })}
-      : String(data)}};
-};
-
-try {
-  var data = JSON.parse(document.body.textContent);
-  document.body.innerHTML = '';
-  jsml([
-    {link: {rel: 'stylesheet', href: chrome.runtime.getURL('jsonv.css')}},
-    {div: {className: 'jsonv', children: jsonv(data), onclick: function(e) {
+  });
+} else {
+  var background = chrome.runtime.connect();
+  //var start = Date.now();
+  background.onMessage.addListener(function(html) {
+    document.body.innerHTML = '<link rel="stylesheet" href="'+chrome.runtime.getURL('jsonv.css')+'"><div class="jsonv">'+html+'</div>';
+    document.onclick = function(e) {
       var t = e.target, c = t.className;
       if (c == 'jsonv-object' || c == 'jsonv-array')
         t.className += ' closed';
       else if (c == 'jsonv-object closed' || c == 'jsonv-array closed')
         t.className = c.split(' ')[0];
-    }}}
-  ], document.body);
-} catch (e) {}
+    };
+    //console.log('time', Date.now()-start);
+  });
+  background.postMessage(document.body.textContent);
+}
