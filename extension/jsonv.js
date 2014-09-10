8617fade92a1d340e8106de684e51691add0b939
Klaus Ganser <kganser@gmail.com>
2014-09-10 10:34:42 -0500

    Allow highlighting, console access

--- extension/jsonv.js
+++ extension/jsonv.js
@@ -11,7 +11,7 @@ if (location.protocol == 'chrome-extension:') {
           return '<span class="jsonv-'+type+'">'+(type == 'array'
             ? '<ol>'+data.map(function(e) { return '<li>'+html(e)+'</li>'; }).join('')+'</ol>'
             : type == 'object'
-              ? '<ul>'+Object.keys(data).map(function(key) { return '<li><span class="jsonv-key">'+key.replace(/&/g, '&amp;').replace(/</g, '&lt;')+'</span>'+html(data[key])+'</li>'; }).join('')+'</ul>'
+              ? '<ul>'+Object.keys(data).map(function(key) { return '<li><span class="jsonv-key">'+key.replace(/&/g, '&amp;').replace(/</g, '&lt;')+'</span>: '+html(data[key])+'</li>'; }).join('')+'</ul>'
               : String(data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/^(https?:\/\/[^\s]+)$/, '<a href="$1">$1</a>')
           )+'</span>';
         }(JSON.parse(json)));
@@ -19,10 +19,13 @@ if (location.protocol == 'chrome-extension:') {
     });
   });
 } else {
-  var background = chrome.runtime.connect();
+  var background = chrome.runtime.connect(),
+      json = document.body.textContent;
   //var start = Date.now();
   background.onMessage.addListener(function(html) {
     document.body.innerHTML = '<link rel="stylesheet" href="'+chrome.runtime.getURL('jsonv.css')+'"><div class="jsonv">'+html+'</div>';
+    //console.log('time', Date.now()-start);
+    document.body.appendChild(document.createElement('script')).textContent = 'json = '+json+';';
     document.onclick = function(e) {
       var t = e.target, c = t.className;
       if (c == 'jsonv-object' || c == 'jsonv-array')
@@ -30,7 +33,7 @@ if (location.protocol == 'chrome-extension:') {
       else if (c == 'jsonv-object closed' || c == 'jsonv-array closed')
         t.className = c.split(' ')[0];
     };
-    //console.log('time', Date.now()-start);
+    json = null;
   });
-  background.postMessage(document.body.textContent);
+  background.postMessage(json);
 }
