6155cbd017c07a3871b9bb6e1b6f00acb1d07e48
Klaus Ganser <kganser@gmail.com>
2014-09-26 20:56:39 -0500

    Show array length when collapsed

--- extension/jsonv.js
+++ extension/jsonv.js
@@ -9,7 +9,7 @@ if (location.protocol == 'chrome-extension:') {
         client.postMessage(function html(data) {
           var type = Array.isArray(data) ? 'array' : typeof data == 'object' ? data ? 'object' : 'null' : typeof data;
           return '<span class="jsonv-'+type+'">'+(type == 'array'
-            ? '<ol>'+data.map(function(e) { return '<li>'+html(e)+'</li>'; }).join('')+'</ol>'
+            ? '<ol><li class="jsonv-count">'+data.length+'</li>'+data.map(function(e) { return '<li>'+html(e)+'</li>'; }).join('')+'</ol>'
             : type == 'object'
               ? '<ul>'+Object.keys(data).map(function(key) { return '<li><span class="jsonv-key">'+key.replace(/&/g, '&amp;').replace(/</g, '&lt;')+'</span>: '+html(data[key])+'</li>'; }).join('')+'</ul>'
               : String(data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/^(https?:\/\/[^\s]+)$/, '<a href="$1">$1</a>')
